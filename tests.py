fafcd41c5c1175d0b8fa9c4752235fe334db91be
Roman Dmitriev <rdmitriev@dealersocket.com>
2019-08-04 21:33:03 +0200

    fix fail test

--- tests.py
+++ tests.py
@@ -8,7 +8,7 @@ class TestStringMethods(unittest.TestCase):
 		tools = Tools()
 		self.assertEqual(tools.plus(1, 2), 3)
 		self.assertEqual(tools.plus(5, 5), 10)
-		self.assertEqual(tools.plus(138, 49), 186)
+		self.assertEqual(tools.plus(137, 49), 186)
 
 	def test_negative(self):
   		tools = Tools()
