2411670dda279e7ea252a6e9462cf8c655d6b1cf
Roman Dmitriev <rdmitriev@dealersocket.com>
2019-08-04 22:19:28 +0200

    add fail test

--- tests.py
+++ tests.py
@@ -7,7 +7,7 @@ class TestStringMethods(unittest.TestCase):
 	def test_positive(self):
 		tools = Tools()
 		self.assertEqual(tools.plus(1, 2), 3)
-		self.assertEqual(tools.plus(5, 5), 10)
+		self.assertEqual(tools.plus(5, 4), 10)
 		self.assertEqual(tools.plus(137, 49), 186)
 
 	def test_negative(self):
